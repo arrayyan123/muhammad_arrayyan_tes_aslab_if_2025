bf4ddbc0f77f2d04e008cd47efdd9cc550cd1619
arrayyan aprilyanto <146199861+arrayyan123@users.noreply.github.com>
2025-08-04 21:04:20 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/build/',
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
@@ -10,4 +11,14 @@ export default defineConfig({
         }),
         react(),
     ],
+    build: {
+        chunkSizeWarningLimit: 2000, 
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom'],
+                },
+            },
+        },
+    },
 });
